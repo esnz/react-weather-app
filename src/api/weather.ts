6698339d7c1ac68968586bb9a5b664d177aef7b5
Ehsan Azizi <ehsanazizi2007@yahoo.com>
2021-08-14 15:49:30 +0430

    improvements

--- src/api/weather.ts
+++ src/api/weather.ts
@@ -1,20 +1,20 @@
 const baseUrl = 'https://api.openweathermap.org/data/2.5';
 
-export const fetchWeatherData = (city: string | { lat: number; lng: number }) => {
+export const fetchWeatherData = async (city: string | { lat: number; lng: number }) => {
   let url = `${baseUrl}/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`;
 
   if (typeof city === 'object') {
     url = `${baseUrl}/weather?lat=${city.lat}&lon=${city.lng}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`;
   }
-  return fetch(url);
+  return await (await fetch(url)).json();
 };
 
-export const fetchExtendedForecastData = (city: string | { lat: number; lng: number }) => {
+export const fetchExtendedForecastData = async (city: string | { lat: number; lng: number }) => {
   let url = `${baseUrl}/forecast/daily?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`;
 
   if (typeof city === 'object') {
     url = `${baseUrl}/forecast/daily?lat=${city.lat}&lon=${city.lng}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`;
   }
 
-  return fetch(url);
+  return await (await fetch(url)).json();
 };
