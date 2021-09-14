186c1f792a7b09f63e2fdc34eb1d81de5428ef4d
Ehsan Azizi <ehsanazizi2007@yahoo.com>
2021-09-14 09:02:50 +0430

    code cleanup

--- src/store/reducers/appReducer.ts
+++ src/store/reducers/appReducer.ts
@@ -37,31 +37,3 @@ const appSlice = createSlice({
 
 export const { changeTempUnit, toggleDarkMode, setIsLoading, setIsInitial } = appSlice.actions;
 export default appSlice.reducer;
-
-// export const appReducer = (state: IAppState = initialState, action: PayloadAction<any, AppActionTypes>) => {
-//   switch (action.type) {
-//     case AppActionTypes.TOGGLE_DARK_MODE:
-//       localStorage.setItem('darkMode', (!state.darkMode).toString());
-//       return {
-//         ...state,
-//         darkMode: !state.darkMode,
-//       };
-//     case AppActionTypes.CHANGE_TEMP_UNIT:
-//       return {
-//         ...state,
-//         tempUnit: state.tempUnit === TempUnit.CELCIUS ? TempUnit.FAHRENHEIT : TempUnit.CELCIUS,
-//       };
-//     case AppActionTypes.SET_IS_LOADING:
-//       return {
-//         ...state,
-//         isLoading: action.payload,
-//       };
-//     case AppActionTypes.SET_IS_INITIAL:
-//       return {
-//         ...state,
-//         isInitial: action.payload,
-//       };
-//     default:
-//       return state;
-//   }
-// };
